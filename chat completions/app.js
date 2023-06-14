b6f7b5ae57e4e5785b022880cb5eec26c98041e6
Zuha-SB <71181437+Zuha-SB@users.noreply.github.com>
2023-06-14 15:32:59 -0500

    new api key and finished chat completions

--- chat completions/app.js
+++ chat completions/app.js
@@ -1,16 +1,18 @@
 const API_KEY = "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"
 
 async function fetchData() {
-    const response = await fetch("https://api.openai.com/v1/completions", {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
         method: "POST",
         headers: {
             Authorization: `Bearer ${API_KEY}`,
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            model: "text-davinci-003",
-            prompt: "hello, how are you today?",
-            max_tokens: 7
+            model: "gpt-3.5-turbo",
+            messages: [{
+                role: "user",
+                content: "Hello!"
+            }]
         })
     })
     const data = await response.json()
