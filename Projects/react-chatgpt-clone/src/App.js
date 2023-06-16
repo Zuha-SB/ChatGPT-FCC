d246d4f051fd44213f415de76edea6db4309d0ac
Zuha-SB <71181437+Zuha-SB@users.noreply.github.com>
2023-06-16 18:07:06 -0500

    Structured webpage with html

--- Projects/react-chatgpt-clone/src/App.js
+++ Projects/react-chatgpt-clone/src/App.js
@@ -1,9 +1,15 @@
 const App = () => {
   return (
     <div className="app">
-      
+      <section className="side-bar"></section>
+      <button>+ New chat</button>
+      <ul className="history"></ul>
+      <nav>
+        <p>Made by Zuha</p>
+      </nav>
+      <section className="main"></section>
     </div>
-  );
+  )
 }
 
 export default App
