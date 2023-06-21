b9b76c493d175a3997137797c3a129a6ef6bf355
Zuha-SB <71181437+Zuha-SB@users.noreply.github.com>
2023-06-21 13:27:52 -0500

    Settting previous chats and titles

--- Projects/react-chatgpt-clone/src/App.js
+++ Projects/react-chatgpt-clone/src/App.js
@@ -1,9 +1,16 @@
+import { useState, useEffect } from 'react'
+
 const App = () => {
+  const [ value, setValue] = useState(null)
+  const [ message, setMessage] = useState(null)
+  const [previousChats, setPreviousChats] = useState([])
+  const [ currentTitle, setCurrentTitle] = useState(null)
+
   const getMessages = async () => {
     const options = {
       method: "POST",
       body: JSON.stringify({
-        message: "hello how are you?"
+        message: value
       }),
       headers: {
         "Content-Type": "application/json"
@@ -12,11 +19,37 @@ const App = () => {
     try {
       const response = await fetch('http://localhost:8000/completions', options)
       const data = await response.json()
-      console.log(data)
+      setMessage(data.choices[0].message)
     } catch (error) {
       console.error(error)
     }
   }
+
+  useEffect(() => {
+    console.log(currentTitle, value, message)
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value)
+    }
+    if (currentTitle && value && message) {
+      setPreviousChats(prevChats => (
+        [...prevChats,
+          {
+            title: currentTitle,
+            role: "user",
+            content: value
+          },
+          {
+            title: currentTitle,
+            role: message.role,
+            content: message.content
+          }
+        ]
+      ))
+    }
+  }, [message, currentTitle])
+
+  console.log(previousChats)
+
   return (
     <div className="app">
       <section className="side-bar">
@@ -27,13 +60,12 @@ const App = () => {
         </nav>
       </section>
       <section className="main">
-        <h1>ZuhaGPT</h1>
+        {!currentTitle && <h1>ZuhaGPT</h1>}
         <ul className="feed">
-
         </ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input/>
+            <input value={value} onChange={(e) => setValue(e.target.value)}/>
             <div id="submit" onClick={getMessages}>➢</div>
           </div>
           <p className="info">
